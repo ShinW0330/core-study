4ea7c329ecf63635ab47e91d1ce371ecf7ff32fa
matai <shinwook0330@gmail.com>
2023-06-05 10:52:28 +0900

    주문과 할인 도메인 실행과 테스트

--- src/main/java/study/core/order/OrderServiceImpl.java
+++ src/main/java/study/core/order/OrderServiceImpl.java
@@ -14,6 +14,7 @@ public class OrderServiceImpl implements OrderService{
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findByid(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
+
         return new Order(memberId,itemName, itemPrice, discountPrice);
     }
 }
