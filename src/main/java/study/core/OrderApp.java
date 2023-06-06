a8fe2f28584da54d2a24ab99e83c550576bdb039
matai <shinwook0330@gmail.com>
2023-06-06 16:31:08 +0900

    MemberApp, OrderApp - ApplicationContext

--- src/main/java/study/core/OrderApp.java
+++ src/main/java/study/core/OrderApp.java
@@ -1,5 +1,7 @@
 package study.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import study.core.member.Grade;
 import study.core.member.Member;
 import study.core.member.MemberService;
@@ -12,9 +14,16 @@ public class OrderApp {
        // MemberService memberService = new MemberServiceImpl();
        // OrderService orderService = new OrderServiceImpl();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService  orderService = appConfig.orderService();
+       // AppConfig appConfig = new AppConfig();
+       // MemberService memberService = appConfig.memberService();
+       // OrderService  orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
