d16484a26de15bf7ed15673cf279572ce6f21820
matai <shinwook0330@gmail.com>
2023-06-06 16:01:48 +0900

    @Configuration, @Bean 스프링 전환

--- src/main/java/study/core/AppConfig.java
+++ src/main/java/study/core/AppConfig.java
@@ -1,5 +1,7 @@
 package study.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import study.core.discount.DiscountPolicy;
 import study.core.discount.RateDiscountPolicy;
 import study.core.member.MemberService;
@@ -8,19 +10,21 @@ import study.core.member.MemoryMemberRepository;
 import study.core.order.OrderService;
 import study.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(discountPolicy(), memberRepository());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
