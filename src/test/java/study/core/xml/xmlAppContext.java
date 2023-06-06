9ef8c5b1479cf12c48a074c66c1874e1bc7162b8
matai <shinwook0330@gmail.com>
2023-06-06 21:43:46 +0900

    xml 기반의 스프링 빈 설정 정보

---
+++ src/test/java/study/core/xml/xmlAppContext.java
@@ -0,0 +1,16 @@
+package study.core.xml;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import study.core.member.MemberService;
+
+public class xmlAppContext {
+    
+    @Test
+    void xmlAppContext(){
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
